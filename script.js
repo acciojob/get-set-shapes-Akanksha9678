bed5953b5d8738c9bf0ecb69343cd0d215075fba
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-03-20 16:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 //complete this code
 class Rectangle {
   constructor(width, height) {
-    if (width <= 0 || height <= 0) {
-      throw new Error("Width and height must be positive integers");
-    }
-    this.width = width;
-    this.height = height;
+    // if (width <= 0 || height <= 0) {
+    //   throw new Error("Width and height must be positive integers");
+    // }
+    this._width = width;
+    this._height = height;
   }
 
   get width() {
-    return this.width;
+    return this._width;
   }
-
+  set width(newWidth){
+	this._width=newWidth;
+  }
   get height() {
-    return this.height;
+    return this._height;
   }
-
+  set height(newHeight){
+	  this._height=newHeight
+  }
   getArea() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 }
 
 class Square extends Rectangle {
   constructor(side) {
-    if (side <= 0) {
-      throw new Error("Side must be a positive integer");
-    }
+	  this._side=side
     super(side, side);
   }
 
   getPerimeter() {
-    return 4 * this.width;
+    return 4 * this._width;
   }
 }
 
